2d7e3dbbe23321beeac7ccd29d367300c81a30e3
Nguyen Hoang Son <nguyenhoangson@example.com>
2016-08-03 22:53:44 +0800

    Test Travis CI

--- example.py
+++ example.py
@@ -1,15 +1,8 @@
 '''
-Example os how to use C.Compiler packages 
+Example of how to use C.Compiler packages 
 '''
 
 from CodeRunner.C.Compiler import Compiler 
  
 c = Compiler()
-
-compiling_result = c.compile("/home/livetolove128/Projects/NTU EdX/packages/CodeRunner/test.c", "/home/livetolove128/Projects/NTU EdX/packages/CodeRunner/test")
-
-result = c.run("/home/livetolove128/Projects/NTU EdX/packages/CodeRunner/test")
-
-print(result)
-print(compiling_result)
 
